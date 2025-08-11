2632c193da94b3cae6ec8f00f2461b7a4baa9fff
RafalKornel <rafal.kornel123@gmail.com>
2025-08-11 22:57:30 +0200

    wip

---
+++ day_3/test.py
@@ -0,0 +1,33 @@
+from main import solution_1, solution_2
+
+
+test_input_1 = "mul(7,8)xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))mul(1,2)"
+test_input_2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))mul(1,2)"
+
+# test_input = "mul(1,2)xmul(3,4)"
+
+
+def test_1():
+    total, operations = solution_1(test_input_1)
+
+    expected = 163 + 56
+
+    assert total == expected, f"Failed! Total: {total}, Operations: {operations}"
+
+    print(total, operations)
+
+def test_2():
+    total, operations = solution_2(test_input_2)
+
+    expected = 50
+
+    assert total == expected, f"Failed! Total: {total}, Operations: {operations}"
+
+    print(total, operations)    
+
+
+if __name__ == "__main__":
+    test_1()
+    test_2()
+
+    print("Tests passed!")
